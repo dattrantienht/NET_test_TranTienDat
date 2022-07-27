ebea9896aa231d9e5d5db84e71a5d01a6e58dcc9
Tran Tien Dat <74689952+dattrantienht@users.noreply.github.com>
2022-07-27 20:03:00 +0700

    Add project files.

---
+++ NET_test_TranTienDat/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NET_test_TranTienDat.Models;
+
+namespace NET_test_TranTienDat.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ NET_test_TranTienDat/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NET_test_TranTienDat.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
